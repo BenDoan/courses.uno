a6574dcf0fb4627774d40be5f38ef32e1bffc881
Ben Doan <ben@bendoan.me>
2017-05-29 21:14:24 -0500

    implemented word cloud caching

--- unomaha_utils/views/teachers.py
+++ unomaha_utils/views/teachers.py
@@ -1,10 +1,27 @@
 import json
+import os
+import tempfile
 
-from flask import Blueprint, request, abort, render_template, redirect, url_for, current_app, make_response
+from flask import (
+    Blueprint,
+    request,
+    abort,
+    render_template,
+    redirect,
+    url_for,
+    current_app,
+    make_response
+)
 from util import term_data, DATA_DIR
+
+from os import path
 
 from utils.teacher_info.teacher_info import get_teacher_info
 from utils.word_cloud.gen_cloud import get_all_words, plot_cloud, clean_text
+
+SCRIPT_DIR = path.dirname(path.realpath(__file__))
+
+WORD_CLOUD_CACHE = path.join(SCRIPT_DIR, "..", "data", "UU_WORDCLOUDS")
 
 teachers = Blueprint('teachers', __name__,
                         template_folder='templates')
@@ -13,9 +30,11 @@ teachers = Blueprint('teachers', __name__,
 def hello():
     return redirect(url_for('teachers.teacher_search_search'))
 
+
 @teachers.route('/search')
 def teacher_search_search():
     return render_template("teacher_search.html")
+
 
 @teachers.route('/view')
 def teacher_search_view():
@@ -33,14 +52,33 @@ def teacher_search_view():
 @teachers.route('/cloud.png')
 def teacher_wordcloud():
     teacher_name = request.args.get('name')
-    current_app.logger.info("Generating word cloud for %s", teacher_name)
+    current_app.logger.info("Getting word cloud for %s", teacher_name)
 
     if not teacher_name:
         abort(400)
 
-    words = get_all_words(DATA_DIR, teacher_name)
-    png = plot_cloud(clean_text(words))
+    cached_png = get_cached_wc(teacher_name)
+    if not cached_png:
+        words = get_all_words(DATA_DIR, teacher_name)
+        png = plot_cloud(clean_text(words))
+        cache_wc(teacher_name, png)
+    else:
+        png = cached_png
 
     response = make_response(png)
     response.headers['Content-Type'] = 'image/png'
     return response
+
+def get_cached_wc(teacher_name):
+    fname = path.join(WORD_CLOUD_CACHE, teacher_name+".png")
+    if path.isfile(fname):
+        with open(fname) as f:
+            return f.read()
+
+def cache_wc(teacher_name, png):
+    if not path.isdir(WORD_CLOUD_CACHE):
+        os.mkdir(WORD_CLOUD_CACHE)
+
+    fname = path.join(WORD_CLOUD_CACHE, teacher_name+".png")
+    with open(fname, "wb") as f:
+        f.write(png)
