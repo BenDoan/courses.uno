82041bc952434d7bab6cf194289cb7cc3d075e0e
Ben Doan <ben@bendoan.me>
2017-07-04 00:18:51 -0500

    added more tests

--- unomaha_utils/test.py
+++ unomaha_utils/test.py
@@ -12,10 +12,12 @@ class TestUnomahaUtils(unittest.TestCase):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
-
     def test_college_view(self):
         result = self.app.get('/classes/view?term=1121&college=ACCT')
         self.assertEqual(result.status_code, 200)
+
+        self.assertIn(b"ACCT", result.data)
+        self.assertIn(b"Spring 2012", result.data)
 
     def test_class_history_view(self):
         result = self.app.get('/classes/history?college=ACCT&course=2010')
@@ -25,9 +27,14 @@ class TestUnomahaUtils(unittest.TestCase):
         result = self.app.get('/rooms/view?term=1121&building=Peter+Kiewit+Institute&room_number=260')
         self.assertEqual(result.status_code, 200)
 
+        self.assertIn(b"Peter Kiewit Institute 260", result.data)
+        self.assertIn(b"Spring 2012", result.data)
+
     def test_teacher_view(self):
         result = self.app.get('/teachers/view?lastname=poss')
         self.assertEqual(result.status_code, 200)
+
+        self.assertIn(b"PRINCIPLES OF ACCOUNTING I", result.data)
 
 if __name__ == '__main__':
   unittest.main()
